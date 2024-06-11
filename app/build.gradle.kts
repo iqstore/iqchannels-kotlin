c159a79a03761b6bccab3b03b04f60dcaa7b4435
Abzal Agabek <abzal.agabek@kaspi.kz>
2024-06-11 18:40:11 +0500

    updated item_my_message.xml @ text width

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "2.0"
+        versionName = "2.0.1-beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
     }

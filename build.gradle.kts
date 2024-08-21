e04e7bb832a8b4ce215c471fd8177666d088e83b
Pavelavl <sfdrus2004@gmail.com>
2024-08-21 16:33:30 +0300

    fix: dropped legacy library & set name maxLen of unsigned user to 100

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("com.android.library") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.jfrog.bintray") version "1.7.3" apply false
     id("com.github.dcendents.android-maven") version "2.0" apply false
 
     `maven-publish`

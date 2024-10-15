df8b25e172c8c150ab4d247d131589c7b4b03311
Pavelavl <sfdrus2004@gmail.com>
2024-10-15 17:25:09 +0300

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "2.0.3"
+        versionName = "2.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
     }

f828b98b1d93a466c849e79283f69ffb14650b76
Mikhail <zinkov04@bk.ru>
2025-04-18 03:34:51 +0300

    SDK-51: Removed adding numbers to the filename

--- iqchannels-sdk/src/main/java/ru/iqchannels/sdk/ui/FileUtils.kt
+++ iqchannels-sdk/src/main/java/ru/iqchannels/sdk/ui/FileUtils.kt
@@ -10,37 +10,9 @@ import java.io.File
 object FileUtils {
 
 	fun createGalleryTempFile(context: Context?, uri: Uri, ext: String?, dir: File? = null): File {
-		var ext = ext
-		var filename = getFileNameFromUri(context, uri)
-
-		if (filename != null) {
-			val i = filename.lastIndexOf(".")
-			if (i > -1) {
-				ext = filename.substring(i + 1)
-				filename = filename.substring(0, i - 1)
-			}
-		} else {
-			filename = "file"
-			val mimeType = context?.contentResolver?.getType(uri)
-			ext = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType)
-		}
-
-		if (filename.length < 3) {
-			filename = "file-$filename"
-		}
-		val file = File.createTempFile(filename, ".$ext", dir ?: context?.cacheDir)
-
-		try {
-			context?.contentResolver?.openInputStream(uri)?.use { inputStream ->
-				file.outputStream().use { outputStream ->
-					inputStream.copyTo(outputStream)
-				}
-			}
-		} catch (e: Exception) {
-			e.printStackTrace()
-			Log.e("createGalleryTempFile", "Failed to copy content from URI to temp file $e")
-		}
-
+		val filename = getFileNameFromUri(context, uri)
+		val file = File((dir ?: context?.cacheDir), "$filename")
+		file.createNewFile()
 		return file
 	}
 

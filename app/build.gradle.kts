d41d1e62172b22072ee5c23f7d2f663972978892
Mikhail <zinkov04@bk.ru>
2025-05-05 16:29:18 +0300

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "2.1.0"
+        versionName = "2.2.0-rc2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
     }

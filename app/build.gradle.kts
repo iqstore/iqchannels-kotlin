4865c928f09ca34e9861e284d201d3c5f6744e01
Mish0kk <zinkov04@bk.ru>
2025-10-01 00:06:55 +0300

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "2.2.5-rc1"
+        versionName = "2.2.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
     }

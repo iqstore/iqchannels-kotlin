fa6e351fbedb172885813902759ff996f65c4758
Mish0kk <zinkov04@bk.ru>
2025-11-27 10:41:36 +0300

    SDK-426: Styled scroll button

--- iqchannels-sdk/src/main/java/ru/iqchannels/sdk/styling/chat/ChatStyles.kt
+++ iqchannels-sdk/src/main/java/ru/iqchannels/sdk/styling/chat/ChatStyles.kt
@@ -2,6 +2,7 @@ package ru.iqchannels.sdk.styling.chat
 
 import com.google.gson.annotations.SerializedName
 import ru.iqchannels.sdk.styling.Color
+import ru.iqchannels.sdk.styling.ContainerStyles
 import ru.iqchannels.sdk.styling.Text
 
 class ChatStyles(
@@ -15,5 +16,9 @@ class ChatStyles(
 	@SerializedName("icon_operator")
 	val iconOperator: String?,
 	@SerializedName("system_text")
-	val systemText: Text?
+	val systemText: Text?,
+	@SerializedName("scroll_down_button_background")
+	val scrollDownButtonBackground: ContainerStyles?,
+	@SerializedName("scroll_down_button_icon_color")
+	val scrollDownButtonIconColor: Color?
 )

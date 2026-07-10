8a9384714f57b8cf5590ab79406e43a17a52ab6f
Mish0kk <zinkov04@bk.ru>
2026-07-10 14:17:39 +0700

    Updated version

--- iqchannels-sdk/src/main/java/ru/iqchannels/sdk/app/IQChannels.kt
+++ iqchannels-sdk/src/main/java/ru/iqchannels/sdk/app/IQChannels.kt
@@ -1698,7 +1698,7 @@ object IQChannels {
 		user.Online = true
 		user.Id = 1
 		val message = ChatMessage(user, localId)
-		message.Text = "2.3.5"
+		message.Text = "2.3.6"
 		messages?.add(message)
 		for (listener in messageListeners) {
 			execute {
